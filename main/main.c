1cab673d9e0f6f0d093c3cb29455a709f5b41f90
Nikola Trifunovic <trifundroid94@gmail.com>
2025-02-23 23:26:11 +0000

    Add sample project

--- main/main.c
+++ main/main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
+#include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-
 void app_main(void)
 {
-    int i = 0;
-    while (1) {
-        printf("[%d] Hello world!\n", i);
-        i++;
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    int count = 0;
+    while (true)
+    {
+        ESP_LOGI("QEMU", "running QEMU count %d", count++);
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
